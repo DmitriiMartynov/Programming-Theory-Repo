896590e6dc42929bd36e99e842df1b228f56c263
DmitriiMartynov <martynovdmitryvladimirovich@yandex.ru>
2022-07-09 16:57:28 +0300

    handle selection initial

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] private Camera gameCamera;
+    [SerializeField] private TextMeshProUGUI description;
+    
     void Update()
     {
-        
+        if (Input.GetMouseButton(0))
+        {
+            // ABSTRACTION
+            handleSelection();
+        }
+    }
+
+    void ShowDescription(BaseFigure figure)
+    {
+        description.text = $"Name: {figure.figureName}\nColor: {figure.color}\nWeight: ";
+        description.text += (figure.weight == int.MaxValue) ? "Infinite" : $"{figure.weight}";
+    }
+
+    void handleSelection()
+    {
+        RaycastHit hit;
+        Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit)) {
+            BaseFigure figure = hit.collider.GetComponentInParent<BaseFigure>();
+            ShowDescription(figure);
+        }
     }
 }
