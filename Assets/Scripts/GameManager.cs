246e29d29df39ab5727ec89ce692d3f39fbe5da0
DmitriiMartynov <martynovdmitryvladimirovich@yandex.ru>
2022-07-10 11:29:27 +0300

    right click on board
    Left click on any figure - select ti
    Right click on the board - show posible movements from the selected position

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -13,7 +13,10 @@ public class GameManager : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
             // ABSTRACTION
-            handleSelection();
+            handleSelection(0);
+        } else if (Input.GetMouseButton(1)) {
+            // ABSTRACTION
+            handleSelection(1);
         }
     }
 
@@ -23,13 +26,18 @@ public class GameManager : MonoBehaviour
         description.text += (figure.weight == int.MaxValue) ? "Infinite" : $"{figure.weight}";
     }
 
-    void handleSelection()
+    void handleSelection(int button)
     {
         RaycastHit hit;
         Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
+
         if (Physics.Raycast(ray, out hit)) {
-            BaseFigure figure = hit.collider.GetComponentInParent<BaseFigure>();
-            ShowDescription(figure);
+            if (hit.collider.gameObject.CompareTag("Figure") && button == 0) {
+                BaseFigure figure = hit.collider.GetComponentInParent<BaseFigure>();
+                ShowDescription(figure);
+            } else if (hit.collider.gameObject.CompareTag("Board") && button == 1) {
+                Debug.Log(gameCamera.ScreenToWorldPoint(Input.mousePosition));
+            }
         }
     }
 }
