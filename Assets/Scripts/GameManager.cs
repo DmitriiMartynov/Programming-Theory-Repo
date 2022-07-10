6dfdf1320e931139765aa92b928b4049ed0d82a2
DmitriiMartynov <martynovdmitryvladimirovich@yandex.ru>
2022-07-10 13:54:39 +0300

    show possible moves

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,16 +7,28 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private Camera gameCamera;
     [SerializeField] private TextMeshProUGUI description;
+    [SerializeField] private GameObject redSphere;
+    [SerializeField] private GameObject blueSphere;
+    List<GameObject> markers;
+
+    BaseFigure selectedFigure;
+    const float cellSize = 0.6f; // TODO: compute using collider
+    // we assume that board is located in (0, 0, ..)
+    float offsetFromCenter = Coordinate.MaxRow / 2 * cellSize;
     
+    void Start()
+    {
+        markers = new List<GameObject>();
+        selectedFigure = GameObject.Find("Chess King White").GetComponent<King>();
+        ShowDescription(selectedFigure);
+    }
+
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             // ABSTRACTION
-            handleSelection(0);
-        } else if (Input.GetMouseButton(1)) {
-            // ABSTRACTION
-            handleSelection(1);
+            handleSelection();
         }
     }
 
@@ -26,18 +38,66 @@ public class GameManager : MonoBehaviour
         description.text += (figure.weight == int.MaxValue) ? "Infinite" : $"{figure.weight}";
     }
 
-    void handleSelection(int button)
+    void handleSelection()
     {
         RaycastHit hit;
         Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit)) {
-            if (hit.collider.gameObject.CompareTag("Figure") && button == 0) {
-                BaseFigure figure = hit.collider.GetComponentInParent<BaseFigure>();
-                ShowDescription(figure);
-            } else if (hit.collider.gameObject.CompareTag("Board") && button == 1) {
-                Debug.Log(gameCamera.ScreenToWorldPoint(Input.mousePosition));
+            if (hit.collider.gameObject.CompareTag("Figure")) {
+                selectedFigure = hit.collider.GetComponentInParent<BaseFigure>();
+                ShowDescription(selectedFigure);
+            } else if (hit.collider.gameObject.CompareTag("Board")) {
+                Vector3 worldPosition = gameCamera.ScreenToWorldPoint(Input.mousePosition);
+                Coordinate coordinate = WorldPointToChessCoordinate(worldPosition);
+
+                RemoveAllMarkers();
+                List<Coordinate> moves = selectedFigure.Move(coordinate);
+
+                // add positions only if the selected coordinate available for the selected figure
+                if (moves.Count > 0)
+                {
+                    // add selected position
+                    AddMarker(redSphere, coordinate);
+                    // add possible move
+                    foreach (Coordinate c in moves)
+                    {
+                        AddMarker(blueSphere, c);
+                    }
+                }
             }
         }
+    }
+
+    void AddMarker(GameObject prefab, Coordinate coordinate)
+    {
+        Vector3 worldPosition = ChessCoordinateToWorldPoint(coordinate);
+        GameObject marker = Instantiate(prefab, worldPosition, prefab.transform.rotation);
+
+        markers.Add(marker);
+    }
+
+    void RemoveAllMarkers()
+    {
+        foreach (GameObject marker in markers)
+        {
+            Destroy(marker);
+        }
+    }
+
+    Coordinate WorldPointToChessCoordinate(Vector3 position)
+    {
+        int column = Mathf.FloorToInt((position.x + offsetFromCenter) / cellSize) + Coordinate.MinColumn;
+        int row = Mathf.FloorToInt((position.y + offsetFromCenter) / cellSize) + Coordinate.MinRow;
+
+        return new Coordinate(column, row);
+    }
+
+    Vector3 ChessCoordinateToWorldPoint(Coordinate coordinate)
+    {
+        float x = (coordinate.column - Coordinate.MinColumn) * cellSize - offsetFromCenter + cellSize / 2;
+        float y = (coordinate.row - Coordinate.MinRow) * cellSize - offsetFromCenter + cellSize / 2;
+
+        return new Vector3(x, y, -0.5f);
     }
 }
